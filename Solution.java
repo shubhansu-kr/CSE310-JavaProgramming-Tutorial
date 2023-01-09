541d8e08a2de66ffa40a137f7a1bf5e57a76e15d
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-09 16:33:46 +0530

    Input files
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ Solution.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution{
+	public static void main(String a[]){
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		System.out.println(num * num);
+	}
+}
