12ec8bab9589d6fbebd8b43196e8e5921ba0ab4c
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-11 15:56:57 +0530

    Practice: Check prime
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/10_Practice.java
+++ 00_Basics/10_Practice.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Solution {
 
-    static boolean isPrime(int num) {
+    boolean static isPrime(int num) {
         if (num < 2) return false;
         for(int i = 2; i <= num/2; ++i) if (num % i == 0) return false;
         return true;
