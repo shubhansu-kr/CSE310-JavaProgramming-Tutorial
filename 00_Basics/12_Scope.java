a7c778b080eeaf8ec42f60ae4571636fb5a13d03
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-16 15:59:58 +0530

    Scope of var
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/12_Scope.java
+++ 00_Basics/12_Scope.java
@@ -1,4 +1,20 @@
+import java.util.*;
+
 class Solution {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // local variable must be initialised or if not initialised must 
+        // not be accessed. 
+        int i;
+
+        // Compile time error: We cannot access an uninitialised local variable.
+        // System.out.println(i);
+
+    }
+}
+
+class Solution1 {
 
     // Constant variable : Use keyword 'final';
     // const variable must be initialised. 
