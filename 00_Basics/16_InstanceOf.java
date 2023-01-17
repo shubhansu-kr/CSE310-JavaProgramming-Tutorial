00f3ce0293837405dce22df1bbb45493a5ec0da0
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-17 15:24:25 +0530

    Instance of operator
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/16_InstanceOf.java
+++ 00_Basics/16_InstanceOf.java
@@ -3,6 +3,10 @@ class Solution {
         Base b = new Base();
         Derived d = new Derived();
 
+        // instanceof operator is used to find the class of object
+        // here, d is the object of derived class, thus it is also
+        // giving true as instance of base class. 
+
         System.out.println(b instanceof Base); // True
         System.out.println(b instanceof Derived); // False
         System.out.println(d instanceof Base); // True
