d03ea1eb26d0b56c6a188658691b31a2ec33eaac
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-17 16:08:22 +0530

    Operators in Java
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/17_Operators.java
+++ 00_Basics/17_Operators.java
@@ -41,3 +41,22 @@ class Solution1 {
     }
 }
 
+class Solution2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 15, b = 21, c = 13;
+
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        c /= a;
+        System.out.println("c /= a = " + c);
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c =- a;
+        System.out.println("c =- a = " + c);
+    }
+}
--- Solution.java
+++ Solution.java
@@ -3,10 +3,19 @@ import java.util.*;
 class Solution {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int c = 21;
-        int a = 15;
+        int a = 15, b = 21, c = 13;
 
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        c /= a;
+        System.out.println("c /= a = " + c);
+        c %= a;
+        System.out.println("c %= a = " + c);
         c =- a;
-        System.out.println(c);
+        System.out.println("c =- a = " + c);
     }
 }
