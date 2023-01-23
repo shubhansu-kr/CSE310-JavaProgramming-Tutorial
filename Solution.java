42444039dec4f72ec6cead1e5184eda7dbece8c9
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-23 15:08:48 +0530

    Added Attachments
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Solution.java
+++ Solution.java
@@ -1,28 +1,10 @@
 import java.util.*;
 
 class Solution {
+    static boolean isOk;
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
-        int age = 18;
-
-        if (age < 18) {
-            System.out.println("GO home");
-        }
-        else if (age == 18) {
-            System.out.println("Sweet");
-        }
-        else {
-            System.out.println("Go Ahead");
-        }
-
-        int i = 10, j = 11, k = 121, a = 1, b = 2, c = 3, d = 4;
-        if (i == 10){
-            if (j < 20) a = b;
-            if (k > 100) c = d;
-            else a = c;
-        }
-
-        System.out.println(a + " " + b + " " + c + " " + i + " " + j + " " + k);
+        System.out.println(isOk);
     }
 }
