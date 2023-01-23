36629672f192e6ab949b1a188ce31ef0f8ada9aa
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-23 15:36:43 +0530

    Practice: Loop
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/23_Loops.java
+++ 00_Basics/23_Loops.java
@@ -25,3 +25,15 @@ class Solution {
         System.out.println("");
     }
 }
+
+class Solution1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        for(; true;){
+            i++;
+            if (i > 10) break;
+            System.out.println(i);
+        }
+    }
+}
