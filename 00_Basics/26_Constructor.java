dd52eb49d5cf7bc8c1f1a6ca03b63e746d44a552
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-24 16:26:18 +0530

    Constructor
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/26_Constructor.java
+++ 00_Basics/26_Constructor.java
@@ -71,3 +71,112 @@ class Solution2 {
         System.out.println(obj3.x + " " + obj3.y);
     }
 }
+
+class Solution3 {
+    // Experiments on constructors access specifier
+    // All the three access specifiers are allowed.
+    // 1. public 
+    // 2. private
+    // 3. protected
+
+    // What are these specifiers.
+    // What is the default access specifiers.
+
+    // public access 
+    public Solution(){
+        System.out.println("Constructor0");
+    }
+
+    // private access 
+    private Solution(int x){
+        System.out.println("Constructor1");
+    }
+
+    // protected access 
+    protected Solution(int x, int y){
+        System.out.println("Constructor2");
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        Solution obj1 = new Solution();
+        Solution obj2 = new Solution(3);
+        Solution obj3 = new Solution(3, 2);
+    }
+}
+
+class Solution4 {
+    static int x, y; // Default value of data members: 0
+
+    // Experiment with variable declaration. 
+    Solution(){
+        System.out.println("Constructor0");
+
+        // Create a non-static variable: Allowed 
+        int temp;
+
+        // swap
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println(x + y);
+    }
+
+    Solution(int x){
+        System.out.println("Constructor1");
+        x += x;
+        y += y;
+
+        // Static declaration of members are not allowed.
+        // static int z = 13;
+    }
+        
+    Solution(int x, int y) {
+        System.out.println("Constructor2");
+
+        // Initialise the static variables. 
+        this.x = x;
+        this.y = y;
+    }
+
+    void display(){
+        System.out.println(x + " " + y);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // Initialise variable x and y
+        Solution obj1 = new Solution(21, 15);
+        obj1.display();
+
+        // Swap x and y
+        Solution obj2 = new Solution();
+        obj1.display();
+
+        // Increase the x and y
+        Solution obj3 = new Solution(1);
+        obj1.display();
+    }
+}
+
+class Solution5 {
+    // Exp: Static keyword with constructor
+
+    // Error: Static modifier not allowed with constructors.
+    // static Solution(){
+    //     System.out.println("Constructor0");
+    // }
+    
+    Solution(){
+        System.out.println("Constructor 0");
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        Solution obj1 = new Solution();
+    }
+}
