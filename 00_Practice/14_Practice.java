a7d1aa88647b4e3669ef7f8d81cde1d3a6f2c0aa
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-01 21:36:49 +0530

    Swap
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Practice/14_Practice.java
+++ 00_Practice/14_Practice.java
@@ -21,3 +21,33 @@ class Solution {
         System.out.println(a + " " + b);
     }
 }
+
+// Use Instance Var
+class Solution1{
+    int a, b;
+
+    void swap(){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    void display(){
+        System.out.println(a + " " + b);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        Solution1 obj = new Solution1();
+        obj.display();
+
+        obj.a = sc.nextInt();
+        obj.b = sc.nextInt();
+
+        
+        obj.display();
+        obj.swap();
+        obj.display();
+    }
+}
--- Solution.java
+++ Solution.java
@@ -4,18 +4,30 @@ import java.util.*;
 
 class Solution {
 
-    static void swap(int a, int b) {
-        int temp = a; 
-        a = b; 
+    int a, b;
+
+    void swap(){
+        int temp = a;
+        a = b;
         b = temp;
+    }
+
+    void display(){
+        System.out.println(a + " " + b);
     }
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
-        int a = sc.nextInt(), b = sc.nextInt();
-        System.out.println(a + " " + b);
-        swap (a, b);
-        System.out.println(a + " " + b);
+        Solution obj = new Solution();
+        obj.display();
+        
+        obj.a = sc.nextInt();
+        obj.b = sc.nextInt();
+
+        obj.display();
+        obj.swap();
+
+        obj.display();
     }
 }
