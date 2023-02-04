5432e8c371d3eb41f03ad922a2858d4d8e911adc
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-04 10:41:46 +0530

    DSA: Array in Java
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ DSA/00_Array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        // All valid way to declare the array.
+        int arr1[];
+        int arr2 [];
+        int []arr3;
+        int [] arr4;
+
+        // Only valid way to declare as well as initialise the array.
+        int arr[] = {1, 3, 4, 2, 4};
+
+        System.out.println(arr.length + " " + arr1.length + " " + arr2.length + " " + arr3.length + " " + arr4.length);
+    }
+}
--- Solution.java
+++ Solution.java
@@ -1,33 +1,41 @@
-// WAP To swap two number using function
-
 import java.util.*;
 
 class Solution {
-
-    int a, b;
-
-    void swap(){
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-
-    void display(){
-        System.out.println(a + " " + b);
-    }
-
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
-        Solution obj = new Solution();
-        obj.display();
+        // All valid way to declare the array.
+        int arr1[];
+        int arr2 [];
+        int []arr3;
+        int [] arr4;
+
+        // Only valid way to declare as well as initialise the array.
+        int arr[] = {1, 3, 4, 2, 4};
+
+        // Error: Cannot access the array which is nor initialised. 
+        // arr1, arr2, arr3, arr4 are not initialised = gives error when we use arr1.length;
+        // System.out.println(arr.length + " " + arr1.length + " " + arr2.length + " " + arr3.length + " " + arr4.length);
+
+        // length is a variable member of array class.
+        System.out.println(arr.length); // prints 5
         
-        obj.a = sc.nextInt();
-        obj.b = sc.nextInt();
+        // Error : Not Initialised.
+        // System.out.println(arr1.length);
+        // System.out.println(arr2.length);
+        // System.out.println(arr3.length);
+        // System.out.println(arr4.length);
 
-        obj.display();
-        obj.swap();
+        // System.out.println(arr1); Error : Not initialised.
 
-        obj.display();
+        // Initialise with new keyword. 
+        // We can initialise the array with size zero.
+        arr1 = new int[0];
+        System.out.println(arr1.length); // prints 0
+        System.out.println(arr1); // prints the address of arr1
+
+        // Error : Index out of bound 
+        // System.out.println(arr1[0]); 
+        // System.out.println(arr1[1]);
     }
 }
