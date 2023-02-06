0a772a0136e284fa6cd0a5c54eefa497ef59a75d
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-06 16:54:01 +0530

    2d array
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/32_TwoDimArray.java
+++ 00_Basics/32_TwoDimArray.java
@@ -2,6 +2,44 @@
 
 import java.util.*;
 
+class Solution1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        // Two-D array in Java
+
+        // In java - Elements of One-D arrays are located at contiguous memory location but
+        // two one D array constituting a 2-D array need not to be contiguous. 
+
+        // int arr[][] = {
+        //                 {1, 2, 3}, 
+        //                 {4, 5, 6}, 
+        //                 {7, 8, 9}
+        //                 };
+
+        // Column Majors : When all the elements are stored column wise.
+        // Col Major seq : 1, 4, 6, 2, 5, 8, 3, 6, 9 ;
+        // Row Major seq : 1, 2, 3, 4, 5, 6, 7, 8, 9 ;
+
+        // Fixed Size: Initialised with default val 0;
+        int arr[][] = new int[4][5]; 
+
+        // Variable Columns Size initialisation | Fixed no. of rows.
+        int arr1[][] = new int[5][];
+        
+        // Using one-D array to initialise the 2D array: Size decided by oneD Array size.
+        int oneD1[] = new int[3];
+        int oneD2[] = new int[15];
+        int oneD2[] = new int[10];
+
+        int arr2[][] = {oneD1, oneD2, oneD3};
+
+        // Using Values to initialise while declaration: 
+        int arr3[][] = {{2,4,3}, {3, 2}, {4,2,3,43,34,3}, {4,3,4,3,43,43,5,4}};
+
+    }
+}
+
 class Solution {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
 # CSE310: JAVA Programming
+
+CSE310 Course Tutorial:
+
+## Documentation
+
+1. []
