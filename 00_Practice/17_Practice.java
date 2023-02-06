800bad10e3ac0a8ffd0df81b04205ea9d4d971bd
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-06 15:51:44 +0530

    Practice
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Practice/17_Practice.java
+++ 00_Practice/17_Practice.java
@@ -4,6 +4,40 @@ import java.util.*;
 
 class Solution {
 
+    // Array Return type.
+    static int[] reverseArray(int arr[]) {
+        int i = 0, j = arr.length - 1;
+        while(i < j) {
+            int temp = arr[i];
+            arr[i++] = arr[j];
+            arr[j--] = temp;
+            // swap(arr[i++], arr[j--]);
+        }
+        return arr;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        System.out.print("Before Swap: ");
+        for(int it: arr){
+            System.out.print(it + " ");
+        }
+        System.out.println();
+        arr = reverseArray(arr);
+        System.out.print("After Swap : ");
+        for(int it: arr){
+            System.out.print(it + " ");
+        }
+
+    }
+}
+
+
+class Solution1 {
+
+    // Void Return type
     static void reverseArray(int arr[]) {
         int i = 0, j = arr.length - 1;
         while(i < j) {
