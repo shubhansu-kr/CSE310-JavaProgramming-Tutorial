9a75216064db3ad235bb4d74887d4a89845b3630
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-07 16:10:48 +0530

    Enum Traverse
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/34_Enums.java
+++ 00_Basics/34_Enums.java
@@ -50,3 +50,14 @@ class Solution {
 enum ReqStatus{
     PENDING, RUNNING, REJECTED, WAITING, RESOLVED;
 }
+
+class Solution1 {
+
+    // Create enum inside Solution class.
+    public enum Week{MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY}
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        for(Week wk: Week.values()) System.out.println(wk);
+    }
+}
--- Solution.java
+++ Solution.java
@@ -1,52 +1,14 @@
-// Enums in Java
+// Enums in java
 
 import java.util.*;
 
 class Solution {
+
+    // Create enum inside Solution class.
+    public enum Week{MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY}
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
-        ReqStatus r = ReqStatus.PENDING;
-        System.out.println(r);
-
-        switch (r) {
-            case PENDING:
-                System.out.println("0");
-                break;
-            case RUNNING: 
-                System.out.println("1");
-                break;
-            default: 
-                System.out.println("def");
-                break;
-        }
-
-        // Enums can be traversed using the values method.
-        for(ReqStatus s: ReqStatus.values()){
-            System.out.println(s);
-        }
-
-        // Values() : Returns an iterable list of all the constants in the enum
-        System.out.println(ReqStatus.values());
-
-        // Enum: 
-        // Group of constants. 
-        // all the user defined enums class are internally inheriting the 
-        // Enum class(Java.lang.enum) from library(Java.lang) which includes default methods like 
-        // values(), getClass(), etc.
-        // We cannot use new keyword with enum because it has private constructor
-        // only.
-
+        for(Week wk: Week.values()) System.out.println(wk);
     }
-}
-
-// All the enum member variable are final(const), static by default(implicitly)
-
-// Syntax: 
-// enum {
-//        Your Constant goes here(seprated by comma);
-// }
-
-enum ReqStatus{
-    PENDING, RUNNING, REJECTED, WAITING, RESOLVED;
 }
