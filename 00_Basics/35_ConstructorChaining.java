3de60391de2eb3d075658dd79df494af6bdeeca6
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-08 15:44:56 +0530

    Constructor Chaining
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_Basics/35_ConstructorChaining.java
+++ 00_Basics/35_ConstructorChaining.java
@@ -3,6 +3,30 @@
 import java.util.*;
 
 class Solution {
+    // D D 21 D 21 21  15
+    Solution() {
+        System.out.println("D");
+    }
+    Solution(int i) {
+        this(); // calls Solution()
+        System.out.println(i);
+    }
+    Solution(int i, int j){
+        this(i);  // calls Solution(i)
+        // this(i); : Error; We cannot add multiple this 
+        System.out.println(i + " " + j);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        Solution obj1 = new Solution();
+        Solution obj2 = new Solution(21);
+        Solution obj3 = new Solution(21, 15);
+    }
+}
+
+class Solution1 {
     // Error: Constructor Recursion invocation
     Solution() {
         System.out.println("D");
