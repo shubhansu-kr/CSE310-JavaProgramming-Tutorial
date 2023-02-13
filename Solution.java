b1c96f67f37d2401b83d22a8aabe40e7965e849f
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-13 15:38:56 +0530

    Practice
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Solution.java
+++ Solution.java
@@ -9,15 +9,19 @@ class Solution {
         // Expecting 3 integer input via command line. 
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
-
+        int flag = 1;
         int n = Integer.parseInt(args[2]);
 
         // Print all the num. between x and y which are divisible ny n
         for (int i = x; i <= y; i++) {
             if (i % n == 0) {
+                flag = 0;
                 System.out.println(i);
             }
         }
-
+        
+        if (flag == 1) {
+            System.out.println("No Output");
+        }
     }
 }
