72e9d7ff7a69952a254cb4aa0db51b65b4d22fdf
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-13 15:49:56 +0530

    Parsing + ValueOf
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_Intermidiate/00_WrapperClass.java
+++ 01_Intermidiate/00_WrapperClass.java
@@ -7,18 +7,37 @@ class Solution {
             System.out.println(args[i]);
         }
 
+        String s = "shubh";
+
         System.out.println(args[0].getClass().getSimpleName());
 
         // We have different static methods to parse the string into different datatype
 
-        int x = Integer.parseInt(args[0]);
-        float f = Float.parseFloat(args[0]);
-        double d = Double.parseDouble(args[0]);
+        int x = Integer.parseInt(s);
+        float f = Float.parseFloat(s);
+        double d = Double.parseDouble(s);
 
+        boolean b = Boolean.parseBoolean(s);
+        long l = Long.parseLong(s);
+        char c = s.charAt(0);
+
+        char ca[] = {'s', 'i', 'i'};
         // but the parsing will only be successfull if the input and parsed type are type compatible.
         // eg. "hello" cannot be parsed into int type. 
 
-        System.out.println(x + f + d);
+        if (b) System.out.println(x + f + d + l);
+
+        // to convert any data type to string we can use valueOf() method.
+
+        String s1 = String.valueOf(x);
+        String s2 = String.valueOf(f);
+        String s3 = String.valueOf(d);
+        String s4 = String.valueOf(l);
+        String s5 = String.valueOf(c);
+        String s6 = String.valueOf(ca);
+        String s7 = String.valueOf(b);
+
+        System.out.println(s1 + s2 + s3 + s4 + s5 + s6 + s7);
 	}
 }
 
