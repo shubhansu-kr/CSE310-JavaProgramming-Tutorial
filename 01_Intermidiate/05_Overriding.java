905f3d90eb006795f5e7b62714ca8d47598f7d17
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-15 15:37:08 +0530

    resolve super and this
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_Intermidiate/05_Overriding.java
+++ 01_Intermidiate/05_Overriding.java
@@ -7,8 +7,8 @@ class Solution {
 
     public static void main(String args[]){
         scientificCalculator Obj = new scientificCalculator();
-        Obj.add(21, 15);
-        
+        System.out.println(Obj.add(21, 15));
+        System.out.println(Obj.difference(21, 15));
     }
 }
 
@@ -21,8 +21,14 @@ class Calculator {
 
 class scientificCalculator extends Calculator {
     int add(int a, int b) {
-        System.out.println("Sum of " + a + " " + b + " is " + super.add(a, b));
 
-        return a+b;
+        System.out.println("Sum of " + a + " " + b + " is " + super.add(a, b));
+        // System.out.println(this.add(a, b)); recursive call 
+        return a+b+10;
+    }
+
+    int difference(int c, int d) {
+        System.out.println("Difference method: " + super.add(c, d));
+        return this.add(c, d)-super.add(c, d);
     }
 }
