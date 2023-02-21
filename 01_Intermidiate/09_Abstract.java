2b46fd503236736b264e548d65d4a3280ea0165f
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-21 15:22:44 +0530

    Abstract Class
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_Intermidiate/09_Abstract.java
@@ -0,0 +1,19 @@
+// Abstract Class in Java
+
+import java.util.*;
+
+class Parent {
+
+}
+
+class Child extends Parent {
+    
+}
+
+class Solution {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]){
+        
+    }
+}
---
+++ 01_Intermidiate/10_StringClass.java
@@ -0,0 +1,11 @@
+// 
+
+import java.util.*;
+
+class Solution {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]){
+        
+    }
+}
