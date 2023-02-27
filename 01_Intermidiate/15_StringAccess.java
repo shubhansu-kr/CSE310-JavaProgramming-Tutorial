a00b9dacca265d12a9f768871e602ba48517eefc
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-27 16:54:03 +0530

    String Access
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_Intermidiate/15_StringAccess.java
@@ -0,0 +1,27 @@
+// 
+
+import java.util.*;
+
+class Solution {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]){
+        
+
+        String s = "Hello";
+
+        // s.charAt(0);
+        // s.setChatAt(0);
+        // s.getChars();
+        // s.append();
+        // s.insert(index, string);
+        // s.setLength();
+
+        // s.delete(start, end index);
+        // s.deleteCharAt(index);
+
+        // s.replace()
+
+        System.out.println(s);
+    }
+}
