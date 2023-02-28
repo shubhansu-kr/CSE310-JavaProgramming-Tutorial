295cb948b27b2e0e618c44ef30f7f0bab1e56fe9
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-28 16:16:19 +0530

    Java Interface Practice
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_Intermidiate/19_InterfaceDump.java
@@ -0,0 +1,148 @@
+// Java program to show that interfaces can
+// have methods from JDK 1.8 onwards
+
+
+// import java.io.*;
+// import java.lang.*;
+// import java.util.*;
+
+
+interface In1 {
+    final int a = 10;
+
+    default void display() {
+        System.out.println("hello");
+    }
+}
+
+// A class that implements the interface.
+class TestClass implements In1 {
+    // Driver Code
+    public static void main(String[] args) {
+        TestClass t = new TestClass();
+        t.display();
+    }
+}
+
+// ---------------------------------------------------------
+
+// Java Program to show that interfaces can
+// have methods from JDK 1.8 onwards
+
+interface In2 {
+    final int a = 10;
+
+    static void display() {
+        System.out.println("hello");
+    }
+}
+
+// A class that implements the interface.
+class TestClass1 implements In2 {
+    // Driver Code
+    public static void main(String[] args) {
+        In2.display();
+    }
+}
+
+// ---------------------------------------------------------
+
+interface A {
+    void method1();
+
+    void method2();
+}
+
+// B now includes method1 and method2
+interface B extends A {
+    void method3();
+}
+
+// the class must implement all method of A and B.
+class gfg implements B {
+    public void method1() {
+        System.out.println("Method 1");
+    }
+
+    public void method2() {
+        System.out.println("Method 2");
+    }
+
+    public void method3() {
+        System.out.println("Method 3");
+    }
+}
+
+// ----------------------------------------------------
+
+interface Student {
+    public void data();
+
+}
+
+class avi implements Student {
+    public void data() {
+        String name = "avinash";
+        int rollno = 68;
+        System.out.println(name);
+        System.out.println(rollno);
+    }
+}
+
+class inter_face {
+    public static void main(String args[]) {
+        avi h = new avi();
+        h.data();
+    }
+}
+
+// ---------------------------------------------------
+
+// implementation Level wise
+
+
+// Level 1
+interface Bank {
+    void deposit();
+
+    void withdraw();
+
+    void loan();
+
+    void account();
+}
+
+// Level 2
+abstract class Dev1 implements Bank {
+    public void deposit() {
+        System.out.println("Your deposit Amount :" + 100);
+    }
+}
+
+abstract class Dev2 extends Dev1 {
+    public void withdraw() {
+        System.out.println("Your withdraw Amount :" + 50);
+    }
+}
+
+// Level 3
+class Dev3 extends Dev2 {
+    public void loan() {
+        System.out.println("Loan from dev3");
+    }
+
+    public void account() {
+        System.out.println("Account from dev3");
+    }
+}
+
+// Level 4
+class Solution1 {
+    public static void main(String[] args) {
+        Dev3 d = new Dev3();
+        d.account();
+        d.loan();
+        d.deposit();
+        d.withdraw();
+    }
+}
--- Solution.java
+++ Solution.java
@@ -1,73 +1,51 @@
-// String Builder 
+// implementation Level wise
 
-import java.util.*;
+// import java.io.*;
+// import java.lang.*;
+// import java.util.*;
 
+//Level 1
+interface Bank {
+    void deposit();
+
+    void withdraw();
+
+    void loan();
+
+    void account();
+}
+
+// Level 2
+abstract class Dev1 implements Bank {
+    public void deposit() {
+        System.out.println("Your deposit Amount :" + 100);
+    }
+}
+
+abstract class Dev2 extends Dev1 {
+    public void withdraw() {
+        System.out.println("Your withdraw Amount :" + 50);
+    }
+}
+
+// Level 3
+class Dev3 extends Dev2 {
+    public void loan() {
+        System.out.println("Loan from dev3");
+    }
+
+    public void account() {
+        System.out.println("account from dev3");
+    }
+}
+
+// Level 4
 class Solution {
-    static Scanner sc = new Scanner(System.in);
-
-    public static void main(String args[]){
-        
-        StringBuilder sb = new StringBuilder();
-        System.out.println(sb.capacity()); // ByDefault 16 bytes capacity is reserved initially. 
-
-        StringBuilder sb1 = new StringBuilder("Java"); // Initialises the Builder with java string. 
-        System.out.println();
-        System.out.println(sb1.length()); // 4
-        System.out.println(sb1.capacity()); // 20 : Default(16) + 4(length of string)
-
-        // Now capacity is 20 and len is 4 so we have 16 bytes to append char
-        // If we cross this 16 byte limit. we will have to reallocate memory 
-        // newCapacity = (oldCapacity + 1) * 2 ;
-        sb1.append("stringofLengthgreaterthanf16.");
-
-        System.out.println();
-        System.out.println(sb1.length()); // 33 length of string. 
-        System.out.println(sb1.capacity()); // 42 capacity : (20 + 1) * 2;
-
-        // Let's say we add string which has length greater than newly alloted mem, from formula, 
-        // we will get capacity equal to the length of the new string. 
-        // ie overflow bytes will be added. 
-        sb1.append("..........................................................................");
-        System.out.println();
-        System.out.println(sb1.length()); // 107
-        System.out.println(sb1.capacity()); // 107  (Capacity same as length)
-
-        StringBuilder s2 = new StringBuilder(20); // capacity 
-        System.out.println();
-        System.out.println(s2.capacity()); // 20
-
-        // We can also use ensureCapacity(n) to set the capacity of the builder
-        s2.ensureCapacity(40);
-        System.out.println();
-        System.out.println(s2.capacity()); // 42 : max(40, (20+1)*2)
-
-        // When we try to increase the capacity of the StringBuilder, the compiler
-        // calculates the new capacity using the formula and compares it with the passed 
-        // capacity and whichever is maximum is used. 
-        // eg. oldCapacity 20; 
-        // sb.ensureCapacity(40);
-        // now the new capcaity according to formula is 42 and passed capacity is 40. so max(42, 40) is
-        // chosen, which in this case is 42.
-        // now again, let's say the sb.ensureCapacity(100) is called. Now formula gives 86 and passed is 100
-        // so new capacity of sb is 100.
-
-        s2.ensureCapacity(100);
-        System.out.println();
-        System.out.println(s2.capacity()); // 100 = max(100, (42+1)*2);
-
-        // s.delete()
-        System.out.println();
-        System.out.println(sb1.length());   // 107
-        System.out.println(sb1.capacity()); // 107
-
-        // s.delete(start, end) : Delete the string from start index to end-1 index.
-        sb1.delete(10, 50);
-
-        System.out.println();
-        System.out.println(sb1.length());   // 67
-        System.out.println(sb1.capacity()); // 107
-
-        // Delete function reduces the length of the string. But the capacity remains the same.
-        
+    public static void main(String[] args) {
+        Dev3 d = new Dev3();
+        d.account();
+        d.loan();
+        d.deposit();
+        d.withdraw();
     }
 }
