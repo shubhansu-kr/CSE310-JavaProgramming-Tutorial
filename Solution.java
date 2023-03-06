063400f4a47eaddcef3712eb1e9ba38adffb9273
Shubhansu kumar <shubhansu2021@gmail.com>
2023-03-06 16:56:06 +0530

    Nested Class in java
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Solution.java
+++ Solution.java
@@ -1,29 +1,44 @@
-// 
+// Nested Class 
 
 import java.util.*;
+
+class Student {
+    // Private Sub-Class
+    private class Marks {
+        // Inner Class : because it is nonstatic.
+
+        // we dont need obj to call the non-static methods inside non-static class
+        void show() {
+            display();
+        }
+    }
+
+    // Public Sub-Class : Static/Non-Static
+    static public class Details {
+        // static nested class. 
+        void show() {
+            System.out.println(name);
+        }
+    }
+
+    static String name = "Shubh";
+    void display() {
+        System.out.println(name + "ansu");
+    }
+
+}
 
 class Solution {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String args[]){
-        char c = 'A';
-        c += 32;
-        System.out.println(c);
+        Student st = new Student();
+        st.display();
+        Student.Details obj = new Student.Details();
+        obj.show();
 
-        String s = "Shubh";
-        String s1 = "";
-        for (int i = 0; i < s.length(); i++) {
-            c = s.charAt(i);
-            if (c >= 'a' && c <= 'z'){
-                System.out.print((char)(c-32));
-                s1 += (char)(c-32);
-            }
-            else {
-                System.out.print(c);
-                s1 += c;
-            }
-        }
-        System.out.println();
-        System.out.println(s1);
+        // Private not available. 
+        // Student.Marks obj1 = new Student.Marks();
+        
     }
 }
