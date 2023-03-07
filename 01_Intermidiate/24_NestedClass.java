e60fe266fa0de721fb5751be10500439366bb88b
Shubhansu kumar <shubhansu2021@gmail.com>
2023-03-07 15:26:58 +0530

    Nested Class
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_Intermidiate/24_NestedClass.java
@@ -0,0 +1,55 @@
+// Nested Class 
+
+import java.util.*;
+
+class Student {
+    // Private Sub-Class
+    private class Marks {
+        // Inner Class : because it is nonstatic.
+
+        // we dont need obj to call the non-static methods inside non-static class
+        void show() {
+            display(); // Outer class memeber fxn. 
+        }
+    }
+
+    // Public Sub-Class : Static/Non-Static
+    static public class Details {
+        // static nested class. 
+        void show() {
+            System.out.println(name);
+            Student s = new Student();
+
+            s.display(); // calls outer member fxn.
+
+            Student.Marks mk = s.new Marks();
+            mk.show(); // call show fxn from Marks nested class.
+
+            // If Marks subClass would have been static, we have a different 
+            // syntax to create the object.
+            // Student.Marks mk = new Student.Marks();
+            
+        }
+    }
+
+    static String name = "Shubh";
+    void display() {
+        System.out.println(name + "ansu");
+    }
+
+}
+
+class Solution {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]){
+        Student st = new Student();
+        st.display();
+        Student.Details obj = new Student.Details();
+        obj.show();
+
+        // Private not available. 
+        // Student.Marks obj1 = new Student.Marks();
+        
+    }
+}
--- Solution.java
+++ Solution.java
@@ -9,7 +9,7 @@ class Student {
 
         // we dont need obj to call the non-static methods inside non-static class
         void show() {
-            display();
+            display(); // Outer class memeber fxn. 
         }
     }
 
@@ -18,6 +18,17 @@ class Student {
         // static nested class. 
         void show() {
             System.out.println(name);
+            Student s = new Student();
+
+            s.display(); // calls outer member fxn.
+
+            Student.Marks mk = s.new Marks();
+            mk.show(); // call show fxn from Marks nested class.
+
+            // If Marks subClass would have been static, we have a different 
+            // syntax to create the object.
+            // Student.Marks mk = new Student.Marks();
+            
         }
     }
 
