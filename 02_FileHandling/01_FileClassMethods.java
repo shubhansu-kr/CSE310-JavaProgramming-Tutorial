8ad9a8fe0684f112d0a6995a1cd590eed3b5773c
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-05 15:57:27 +0530

    File Class Methods in Java
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_FileHandling/01_FileClassMethods.java
+++ 02_FileHandling/01_FileClassMethods.java
@@ -19,31 +19,31 @@ class Solution {
         // long length();
 
         File F0 = new File("Sample.txt");
-        File F1 = new File("Sample");
+        File F1 = new File("00_Basics");
 
-        System.out.println(F0.isFile()); // T
-        System.out.println(F1.isFile()); // F
+        System.out.println(F0.isFile());
+        System.out.println(F1.isFile());
         
-        System.out.println(F0.isDirectory()); // F
-        System.out.println(F1.isDirectory()); // T
+        System.out.println(F0.isDirectory());
+        System.out.println(F1.isDirectory());
 
-        System.out.println(F0.exists()); // T
-        System.out.println(F1.exists()); // T
+        System.out.println(F0.exists());
+        System.out.println(F1.exists());
 
-        System.out.println(F0.canRead()); // T
-        System.out.println(F1.canRead()); // T
+        System.out.println(F0.canRead());
+        System.out.println(F1.canRead());
         
-        System.out.println(F1.canWrite()); // T
-        System.out.println(F1.canWrite()); // T
+        System.out.println(F1.canWrite());
+        System.out.println(F1.canWrite());
 
-        System.out.println(F0.getName()); 
-        System.out.println(F1.getName()); 
+        System.out.println(F0.getName());
+        System.out.println(F1.getName());
         
         System.out.println(F0.getPath());
         System.out.println(F1.getPath());
 
-        System.out.println(F0.getAbsolutePath());
-        System.out.println(F1.getAbsolutePath());
+        System.out.println(F0.getPath());
+        System.out.println(F1.getPath());
 
         System.out.println(F0.lastModified());
         System.out.println(F1.lastModified());
