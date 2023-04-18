2e82790b230da8aca32648cb979bc70c893a54cd
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-18 16:55:24 +0530

    Iterator: Java
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 04_Collections/02_Iterator.java
+++ 04_Collections/02_Iterator.java
@@ -15,12 +15,25 @@ class Solution {
         nums.add(9);
         nums.add(10);
 
+        System.out.println(nums);
+
         Iterator<Integer> it = nums.iterator();
-
-        System.out.println(it.next());
-
+        // System.out.println(it.next());
+        // int i = 0;
         while(it.hasNext()){
-            System.out.println(it.next());
+            // System.out.println(it.next());
+            int x = it.next();
+            if (x > 7 && x < 10){
+                it.remove();
+            }
         }
+
+        System.out.println(nums);
+
+        // it.next(): Prints the current element and moves the iterator to next
+        // it.hasNext(): Checks if next element exitsts or not. Returns a boolean val.
+
+        // it.forEachRemaining();
+
     }
 }
