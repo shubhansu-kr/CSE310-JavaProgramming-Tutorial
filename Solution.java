a6fda0245e17339fe761ef05b8c31203b5792e92
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-24 21:49:48 +0530

    Solution
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Solution.java
+++ Solution.java
@@ -1,5 +1,4 @@
-// Use ListIterator Method to replace every occurence of 0 with 1
-// Also Enclose every group of 10 with 20.
+// 
 
 import java.util.*;
 
@@ -7,32 +6,6 @@ class Solution {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String args[]){
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-
-        for (int i = 0; i < 10; i++) {
-            nums.add(sc.nextInt());
-        }
-
-        System.out.println(nums);
-
-        ListIterator<Integer> it = nums.listIterator();
-
-        while(it.hasNext()) {
-            int x = it.next();
-
-            if (x == 10) {
-                it.previous();
-                it.add(20);
-                while(it.hasNext() && it.next() == 10);
-                it.previous();
-                it.add(20);
-                it.next();
-            }
-            else if (x == 0) {
-                it.set(1);
-            }
-        }
-
-        System.out.println(nums);
+        System.out.println("Sout");
     }
 }
