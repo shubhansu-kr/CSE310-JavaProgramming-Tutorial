dd75403c2aa34ff8eb0ad3b0fe9345134a6b8646
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-26 15:55:07 +0530

    Collection.Sort
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Solution.java
+++ Solution.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-class Student {
+class Student implements Comparable<Student>{
     int roll;
     String name;
 
@@ -10,6 +10,18 @@ class Student {
     Student(int roll, String name) {
         this.roll = roll;
         this.name = name;
+    }
+
+    // implement comparator function in class 
+    @Override
+    public int compareTo(Student o) {
+        if (this.roll > o.roll) {
+            return 1;
+        }
+        else if (this.roll < o.roll) {
+            return -1;
+        }
+        return 0;
     }
 }
 
@@ -27,35 +39,47 @@ class Solution {
         Collections.sort(al);
 
         ArrayList<Student> std = new ArrayList<Student>();
-        std.add(new Student(0, "shubh"));
+        std.add(new Student(3, "shubh"));
         std.add(new Student(1, "Nitin"));
-        std.add(new Student(3, "Mukher"));
-        std.add(new Student(4, "Lakh"));
+        std.add(new Student(8, "Mukher"));
+        std.add(new Student(6, "Lakh"));
 
         // System.out.println(std);
         for (Student itrStudent : std) {
-            System.out.println((itrStudent.name));
+            System.out.print((itrStudent.name) + " ");
         }
+        System.out.println();
+        
+        // Sorting on the basis of implemented 
+        // Sort by roll
+        Collections.sort(std);
+        
+        for (Student itrStudent : std) {
+            System.out.print((itrStudent.name) + " ");
+        }
+        System.out.println();
 
         // std.sort(); : Need to pass the comparator
         // Collections.sort(std); : Error: Need to overwrite compareTo method
         // to make custom comparator.
 
         Comparator<Student> compareTo = (Student obj1, Student obj2)->{
-            if (obj1.name.charAt(0) > obj2.name.charAt(0)) {
+            if (obj1.name.length() > obj2.name.length()) {
                 return 1;
             }
-            else if (obj1.name.charAt(0) < obj2.name.charAt(0)) {
+            else if (obj1.name.length() < obj2.name.length()) {
                 return -1;
             }
             return 0;
         };
 
+        // Sort by length of name
         Collections.sort(std, compareTo);
 
         for (Student itrStudent : std) {
-            System.out.println((itrStudent.name));
+            System.out.print((itrStudent.name)+" ");
         }
+        System.out.println();
 
         // System.out.println(std);
     }
