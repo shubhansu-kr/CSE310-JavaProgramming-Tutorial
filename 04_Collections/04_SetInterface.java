d8546effd98509e5c46628c8ada6f63894a0f2dc
Shubhansu kumar <shubhansu2021@gmail.com>
2023-05-08 11:13:40 +0530

    HashSet: Java
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 04_Collections/04_SetInterface.java
@@ -0,0 +1,15 @@
+// SetInterface
+
+import java.util.*;
+
+class Solution {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]){
+        HashSet<Integer> st = new HashSet<Integer>();
+
+        st.add(4);
+        st.add(4);
+        
+    }
+}
